16b044dea3bfcab339e3454f96c367f08169603a
Pallugowda16 <pallavi.s2@s.amity.edu>
2025-02-18 19:50:03 +0530

    even or odd

---
+++ evenodd.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void eveenorodd()
+{
+   int num;
+   scanf("%d",&num);
+   if(num%2==0)
+   {printf("the no is even");}
+   else{printf("the number is odd");}
+ }
+ void main()
+ {even or odd();}
