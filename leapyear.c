b3ddfe0d7582cd48eae0e1e92745736fdff19a4f
Pallugowda16 <pallavi.s2@s.amity.edu>
2025-03-07 21:26:32 +0530

    completed till 2nd if else stmnt

--- leapyear.c
+++ leapyear.c
@@ -3,3 +3,9 @@ int main() {
    int year;
    printf("Enter a year: ");
    scanf("%d", &year);
+   if (year % 400 == 0) {
+      printf("%d is a leap year.", year);
+   }
+   else if (year % 100 == 0) {
+      printf("%d is not a leap year.", year);
+   }
