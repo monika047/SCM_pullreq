a210c0fdd36dc3425a70c23bcad0954b5c90bac3
Pallugowda16 <pallavi.s2@s.amity.edu>
2025-03-19 13:37:57 +0530

    pg done

--- 19thMarch/swap2num.c
+++ 19thMarch/swap2num.c
@@ -4,3 +4,11 @@ int main() {
   printf("Enter first number: ");
   scanf("%lf", &first);
   printf("Enter second number: ");
+  scanf("%lf", &second);
+  temp = first;
+  first = second;
+  second = temp;
+  printf("\nAfter swapping, first number = %.2lf\n", first);
+  printf("After swapping, second number = %.2lf", second);
+  return 0;
+}
