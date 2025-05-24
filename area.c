0234da7a76b4908b2d42522c6dad52e96d047d6b
monika047 <monikaggopalappa@gmail.com>
2025-05-24 02:06:28 -0400

    c1

--- area.c
+++ area.c
@@ -5,6 +5,7 @@ void main()
 	float area,pi=3.14;
 	int r;
 	printf("enter the radius:");
+	printf("enter the radius1:");
 	scanf("%d"&r);
         area=pi*r*r;
 	printf("the area of the circle for given radius is:%f",area);
